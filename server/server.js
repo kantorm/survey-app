362dd16360f64e49325ae7eca03df2a490783ef5
Michał Kantor <kantorm96@gmail.com>
2017-09-17 16:00:11 +0200

    node api server setup (#3)

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import router from "./routes";
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost/survey_app", {
+  useMongoClient: true,
+});
+
+// Initialize http server
+const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Logger that outputs all requests into the console
+app.use(morgan("combined"));
+// Use v1 as prefix for all API endpoints
+app.use("/v1", router);
+
+const server = app.listen(3000, () => {
+  const { address, port } = server.address();
+  console.info(`Listening at http://${address}:${port}`);
+});
